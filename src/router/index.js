3655758d3081217cc40d6ce695a31affff2f0cc7
FixPng <897491068@qq.com>
2023-03-29 00:59:38 +0800

    前端框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/login",
+            name: "login",
+            component: () => import("../views/login.vue")
+        }, {
+            path: "/admin",
+            name: "admin",
+            component: () => import("../views/admin/admin.vue"),
+            children: []
+        }
+    ]
 })
 
 export default router
---
+++ src/views/admin/admin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gvb_admin">
+    <aside>
+
+    </aside>
+    <div class="main">
+      <header></header>
+      <div class="tabs"></div>
+      <main></main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+.gvb_admin{
+  width:100%;
+aside {
+    width: 240px;
+    height: 100vh;
+    background-color: #2b3539;
+  }
+}
+
+</style>
