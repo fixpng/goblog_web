d19dff25a613538526e82293484db1ffc187b970
FixPng <897491068@qq.com>
2023-04-01 01:13:44 +0800

    下拉菜单点击事件

--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -31,17 +31,17 @@
                 <i class="fa fa-angle-down"></i>
               </a>
               <template #overlay>
-                <a-menu>
-                  <a-menu-item>
+                <a-menu @click="menuClick">
+                  <a-menu-item key="user_center">
                     <a href="javascript:;">个人中心</a>
                   </a-menu-item>
-                  <a-menu-item>
+                  <a-menu-item key="my_messages">
                     <a href="javascript:;">我的消息</a>
                   </a-menu-item>
-                  <a-menu-item>
+                  <a-menu-item key="article_list">
                     <a href="javascript:;">文章列表</a>
                   </a-menu-item>
-                  <a-menu-item>
+                  <a-menu-item key="logout">
                     <a href="javascript:;">注销退出</a>
                   </a-menu-item>
                 </a-menu>
@@ -57,6 +57,19 @@
 </template>
 
 <script setup>
+import {useRouter} from "vue-router"
+
+const router = useRouter()
+
+function menuClick({key}) {
+  if (key === "logout") {
+    console.log("logout")
+    return
+  }
+  router.push({
+    name: key
+  })
+}
 </script>
 
 <style lang="scss">
@@ -108,7 +121,8 @@
           border-radius: 50%;
         }
       }
-      .drop_menu{
+
+      .drop_menu {
         margin-left: 10px;
       }
     }
