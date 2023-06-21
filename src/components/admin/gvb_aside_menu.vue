71f770617d22b4419ee1a4fda447c9e68ae69f42
FixPng <897491068@qq.com>
2023-06-22 00:17:01 +0800

    后台：导航菜单优化

--- src/components/admin/gvb_aside_menu.vue
+++ src/components/admin/gvb_aside_menu.vue
@@ -2,25 +2,22 @@
   <a-menu
       v-model:selectedKeys="selectedKeys"
       mode="inline"
-      multiple
       :inline-collapsed="false"
-      @click="goto"
-      @openChange="onOpenChange"
       :open-keys="data.openKeys"
   >
     <template v-for="menu in data.menuList" :key="menu.name">
-      <a-menu-item :key="menu" v-if="menu.children.length === 0">
+      <a-menu-item :key="menu.name" v-if="menu.children.length === 0">
         <template #icon>
           <i :class="'fa '+menu.icon"></i>
         </template>
         <span>{{ menu.title }}</span>
       </a-menu-item>
-      <a-sub-menu :key="menu.id" v-else>
+      <a-sub-menu :key="menu.name" v-else>
         <template #icon>
           <i :class="'fa '+menu.icon"></i>
         </template>
         <template #title>{{ menu.title }}</template>
-        <a-menu-item v-for="sub_menu in menu.children" :key="sub_menu">
+        <a-menu-item v-for="sub_menu in menu.children" @click="goto(sub_menu)" :key="sub_menu.name">
           <template #icon>
             <i :class="'fa '+sub_menu.icon"></i>
           </template>
@@ -49,32 +46,32 @@ const data = reactive({
       children: []
     },
     {
-      id: 23,
-      icon: "fa-home",
+      id: 2,
+      icon: "fa-user-circle-o",
       title: "个人中心",
       name: "user_center",
       children: [
         {
-          id: 24,
-          icon: "fa-home",
+          id: 21,
+          icon: "fa-vcard",
           title: "我的信息",
           name: "user_info",
         },
         {
-          id: 25,
-          icon: "fa-home",
+          id: 22,
+          icon: "fa-user-plus",
           title: "我的发布",
           name: "user_article_list",
         },
         {
-          id: 26,
-          icon: "fa-home",
+          id: 23,
+          icon: "fa-heart",
           title: "我的收藏",
           name: "user_collects",
         },
         {
-          id: 27,
-          icon: "fa-home",
+          id: 24,
+          icon: "fa-desktop",
           title: "我的消息",
           name: "user_messages",
         }
@@ -82,100 +79,105 @@ const data = reactive({
     },
     {
       id: 3,
-      icon: "fa-users",
-      title: "用户列表",
-      name: "user_list",
-      children: []
-    }
-    ,
-    {
-      id: 5,
-      icon: "fa-users",
-      title: "图片列表",
-      name: "image_list",
-      children: []
-    }
-    ,
-
-    {
-      id: 7,
-      icon: "fa-users",
-      title: "广告列表",
-      name: "advert_list",
-      children: []
-    }
-    ,
-
-    {
-      id: 9,
-      icon: "fa-users",
-      title: "菜单列表",
-      name: "menu_list",
-      children: []
-    }
-    ,
-    {
-      id: 11,
-      icon: "fa-users",
-      title: "日志列表",
-      name: "log_list",
-      children: []
-    }
-    ,
-    {
-      id: 12,
-      icon: "fa-users",
-      title: "群聊消息",
-      name: "chat_list",
-      children: []
-    }
-    ,
-    {
-      id: 13,
-      icon: "fa-users",
-      title: "消息列表",
-      name: "message_list",
-      children: []
-    }
-    ,
-    {
-      id: 14,
-      icon: "fa-users",
-      title: "文章列表",
-      name: "article_list",
-      children: []
-    }
-    ,
-    {
-      id: 22,
-      icon: "fa-users",
-      title: "添加文章",
-      name: "add_article",
-      children: []
-    }
-    ,
-    {
-      id: 15,
-      icon: "fa-users",
-      title: "标签列表",
-      name: "tag_list",
-      children: []
-    }
-    ,
-    {
-      id: 16,
-      icon: "fa-users",
-      title: "系统管理",
-      name: "",
+      icon: "fa-book",
+      title: "文图管理",
+      name: "wen_tu",
       children: [
         {
-          id: 17,
+          id: 31,
+          icon: "fa-graduation-cap",
+          title: "添加文章",
+          name: "add_article",
+          children: []
+        },
+        {
+          id: 32,
+          icon: "fa-gift",
+          title: "文章管理",
+          name: "article_list",
+          children: []
+        },
+        {
+          id: 33,
+          icon: "fa-file-image-o",
+          title: "图片管理",
+          name: "image_list",
+          children: []
+        },
+        {
+          id: 34,
+          icon: "fa-comment",
+          title: "评论管理",
+          name: "comment_list",
+          children: []
+        }
+      ]
+    },
+    {
+      id: 4,
+      icon: "fa-male",
+      title: "用户管理",
+      name: "users",
+      children: [
+        {
+          id: 41,
           icon: "fa-users",
+          title: "用户列表",
+          name: "user_list",
+          children: []
+        },
+        {
+          id: 42,
+          icon: "fa-group",
+          title: "群聊消息",
+          name: "chat_list",
+          children: []
+        },
+        {
+          id: 43,
+          icon: "fa-user-circle",
+          title: "消息列表",
+          name: "message_list",
+          children: []
+        },
+      ]
+    },
+    {
+      id: 5,
+      icon: "fa-cogs",
+      title: "系统管理",
+      name: "sys",
+      children: [
+        {
+          id: 51,
+          icon: "fa-tree",
+          title: "菜单列表",
+          name: "menu_list",
+          children: []
+        },
+        {
+          id: 52,
+          icon: "fa-sun-o",
+          title: "广告列表",
+          name: "advert_list",
+          children: []
+        },
+
+        {
+          id: 53,
+          icon: "fa-server",
+          title: "系统日志",
+          name: "log_list",
+          children: []
+        },
+        {
+          id: 54,
+          icon: "fa-globe",
           title: "项目配置",
           name: "system",
         },
       ]
-    }
+    },
   ],
   openKeys: []
 })
@@ -183,37 +185,30 @@ const selectedKeys = ref([])
 const router = useRouter()
 const route = useRoute()
 
-function goto({item, key, keyPath}) {
+function goto(item) {
   store.addTab({
-    name: key.name,
-    title: key.title
+    name: item.name,
+    title: item.title
   })
   // 加入到 tabs
   router.push({
-    name: key.name
+    name: item.name
   })
 }
 
-// 菜单栏只展开一个
-function onOpenChange(openKeys) {
-  const latestOpenKey = openKeys.find(key => data.openKeys.indexOf(key) === -1);
-  data.openKeys = latestOpenKey ? [latestOpenKey] : [];
-}
 
 function loadRoute(name) {
-  if (name === undefined) {
-    name = route.name
-  }
+
   for (const menu of data.menuList) {
     if (menu.name === name) {
-      selectedKeys.value = [menu]
+      selectedKeys.value = [menu.name]
       return
     }
     for (const subMenu of menu.children) {
       if (subMenu.name === name) {
-        selectedKeys.value = [subMenu]
+        selectedKeys.value = [subMenu.name]
         // 找到id，展开对应的菜单
-        data.openKeys = [menu.id]
+        data.openKeys = [menu.name]
         return
       }
     }
@@ -225,7 +220,7 @@ onBeforeRouteUpdate((to, from, next) => {
   next()
 
 })
-loadRoute()
+loadRoute(route.name)
 
 </script>
 
